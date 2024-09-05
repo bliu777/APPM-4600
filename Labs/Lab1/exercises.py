ab6264c8a690e47610e7d81bf87023bb7a65ec5b
Byron Liu <byli4778@colorado.edu>
2024-09-05 13:58:05 -0600

    added comments to lab1 code

--- Labs/Lab1/exercises.py
+++ Labs/Lab1/exercises.py
@@ -1,12 +1,16 @@
+#import relevant libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
+#create identical sets of integers from 1-10
 X = np.linspace(1, 10, 10)
 Y = np.arange(1, 10, 1)
 
+#Print X and Y to verify they're the same
 print(X)
 print(Y)
 
+#Print the first 3 values of X
 print('The first 3 values of X are: ', X[0], X[1], X[2])
 
 
